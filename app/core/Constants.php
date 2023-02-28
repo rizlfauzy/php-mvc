b029e1f2948db4b284b240bd4f7489248ae81661
Rizal Fauzi <rizalfauzi774@gmail.com>
2023-02-28 18:56:22 +0700

    [IGNORE] Delete Constants.php

--- app/core/Constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("BASE_URL", "http://localhost/php-mvc/public");
-define("HOST","localhost");
-define("PORT",5432);
-define("DB_NAME","php-mvc");
-define("DB_USER","postgres");
-define("DB_PASS","fbsteam");
